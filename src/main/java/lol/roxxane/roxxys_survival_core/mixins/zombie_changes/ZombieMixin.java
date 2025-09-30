16893c97d1f551a1c480905cff2cecc8233ff487
Creature Girl <wolfboycoolkid@gmail.com>
2025-09-29 22:27:11 -0500

    ACTUALLY removed natural zombie armor

--- src/main/java/lol/roxxane/roxxys_survival_core/mixins/zombie_changes/ZombieMixin.java
+++ src/main/java/lol/roxxane/roxxys_survival_core/mixins/zombie_changes/ZombieMixin.java
@@ -1,6 +1,6 @@
 package lol.roxxane.roxxys_survival_core.mixins.zombie_changes;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.monster.Zombie;
@@ -9,9 +9,8 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Zombie.class)
 abstract class ZombieMixin {
-	@ModifyExpressionValue(method = "createAttributes",
-		at = @At(value = "INVOKE",
-			target = "Lnet/minecraft/world/entity/monster/Monster;createMonsterAttributes()Lnet/minecraft/world/entity/ai/attributes/AttributeSupplier$Builder;"))
+	@ModifyReturnValue(method = "createAttributes",
+		at = @At(value = "RETURN"))
 	private static AttributeSupplier.Builder changeMaxHealth(AttributeSupplier.Builder original) {
 		return original.add(Attributes.MAX_HEALTH, 10).add(Attributes.ARMOR, 0);
 	}
